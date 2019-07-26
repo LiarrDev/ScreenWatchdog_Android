8a43e396570440b367c97d2e61ec643bab00cc0d
Liarr <liarr@foxmail.com>
2019-07-26 18:48:47 +0800

    Redesign some UI

--- app/src/main/java/com/liarr/screenwatchdog/AboutActivity.java
+++ app/src/main/java/com/liarr/screenwatchdog/AboutActivity.java
@@ -1,10 +1,16 @@
 package com.liarr.screenwatchdog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    TextView webSiteLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +18,18 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        webSiteLink = findViewById(R.id.website_link);
+
+        webSiteLink.setText(Html.fromHtml("Visit my Website: <u><font color='#283449'><big>https://liarrdev.github.io</big></font></u>"));
+
+        webSiteLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutActivity.this, WebActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/liarr/screenwatchdog/WebActivity.java
@@ -0,0 +1,73 @@
+package com.liarr.screenwatchdog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class WebActivity extends AppCompatActivity {
+
+    WebView webView;
+    ProgressBar progressBar;
+
+    String blogUrl = "https://liarrdev.github.io";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
+
+        progressBar = findViewById(R.id.progress_bar);
+        webView = findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                }
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // 获取 WebView 内网页的 Title 并在 Toolbar 上显示
+                String title = view.getTitle();
+                if (!TextUtils.isEmpty(title)) {
+                    getSupportActionBar().setTitle(title);
+                }
+            }
+        });
+        webView.loadUrl(blogUrl);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
